2c0c320385a5fec67c04e379a748ed8c3d181233
xiahongjian <hongjian.xia@qq.com>
2021-01-23 23:20:48 +0800

    :sparkles: 菜单API

--- frontend/src/api/menu.js
+++ frontend/src/api/menu.js
@@ -1,8 +1,51 @@
 import request from '@/utils/request'
 
+const BASE = '/menu'
+
 export function getRoutes() {
   return request({
     url: 'menu/menurole',
     method: 'get'
+  })
+}
+
+export function listMenu(query, status) {
+  return request({
+    url: '/menus',
+    method: 'get',
+    params: {
+      query,
+      status
+    }
+  })
+}
+
+export function createMenu(data) {
+  return request({
+    url: BASE,
+    method: 'post',
+    data
+  })
+}
+
+export function updateMenu(id, data) {
+  return request({
+    url: `${BASE}/${id}`,
+    method: 'put',
+    data
+  })
+}
+
+export function getMenu(id) {
+  return request({
+    url: `${BASE}/${id}`,
+    method: 'get'
+  })
+}
+
+export function deleteMenu(id) {
+  return request({
+    url: `${BASE}/${id}`,
+    method: 'delete'
   })
 }
