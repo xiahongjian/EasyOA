b1d7bfa26912bc990edb95525689332f27b5e9d1
xiahongjian <hongjian.xia@qq.com>
2021-03-18 22:15:10 +0800

    :boom: 添加组件

---
+++ frontend/src/components/StatusRadioGroup/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-radio-group v-bind="$attrs" v-on="$listeners">
+    <el-radio
+      v-for="s in statusOptions"
+      :key="s.value"
+      :label="s.value"
+    >{{ s.label }}</el-radio>
+  </el-radio-group>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'StatusRadioGroup',
+  computed: {
+    ...mapGetters(['statusOptions'])
+  }
+}
+</script>
---
+++ frontend/src/components/StatusTag/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-tag
+    :type="status === 0 ? 'danger' : 'success'"
+    disable-transitions
+    v-bind="$attrs"
+  >{{ selectDictLabel(statusOptions, status) }}</el-tag>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'StatusTag',
+  props: {
+    status: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    ...mapGetters(['statusOptions'])
+  }
+}
+</script>
