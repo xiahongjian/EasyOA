690d318e6433fbc054fee5cb6f98c86333fa2676
xiahongjian <hongjian.xia@qq.com>
2021-03-20 23:31:16 +0800

    :sparkles: 添加流程模型管理页面

---
+++ frontend/src/views/sys/process/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <basic-layout>
+    <template #wrapper>
+      <el-card class="box-card">
+        <el-form ref="queryForm" :inline="true" :model="queryParams" />
+      </el-card>
+    </template>
+  </basic-layout>
+</template>
+
+<script>
+export default {
+  name: 'ProcessModel',
+  data() {
+    return {
+      queryPrams: {
+        key: undefined,
+        name: undefined,
+        page: 0,
+        limit: 10
+      }
+    }
+  },
+  created() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
